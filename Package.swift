1d237efc69dbf2346f8bd9719f795bb5d1bc82c9
Théo Cauffour <theo.c@apps-panel.com>
2025-06-10 03:00:45 +0200

    5.6.0

--- Package.swift
+++ Package.swift
@@ -10,9 +10,6 @@ let package = Package(
             name: "AppsPanelSDK",
             targets: ["AppsPanelSDK"]),
         .library(
-            name: "AppsPanelSDKCore",
-            targets: ["AppsPanelSDKCore"]),
-        .library(
             name: "AppsPanelSDKExtension",
             targets: ["AppsPanelSDKExtension"]),
     ],
@@ -23,39 +20,25 @@ let package = Package(
         .package(url: "https://github.com/DataDog/dd-sdk-ios.git", .exact("2.26.0")),
     ],
     targets: [
-        .target(
-            name: "AppsPanelSDKCore",
-            dependencies: [
-                .product(name: "Alamofire", package: "Alamofire"),
-                .product(name: "KeychainAccess", package: "KeychainAccess"),
-                .product(name: "SwiftJWT", package: "Swift-JWT"),
-            ],
-            path: "Sources/AppsPanelSDK",
-            exclude: [
-                "Logger/DatadogLogger.swift",
-                "Logger/Logger.swift"
-            ],
-            resources: [.process("Resources")]
-        ),
-        .target(
-            name: "AppsPanelSDK",
-            dependencies: [
-                "AppsPanelSDKCore",
-                .product(name: "DatadogCore", package: "dd-sdk-ios"),
-                .product(name: "DatadogLogs", package: "dd-sdk-ios")
-            ],
-            path: "Sources/AppsPanelSDK",
-            sources: [
-                "Logger/DatadogLogger.swift",
-                "Logger/Logger.swift"
-            ],
-            resources: [.process("Resources")]
-        ),
-        .target(
-            name: "AppsPanelSDKExtension",
-            dependencies: ["AppsPanelSDKCore"],
-            path: "Sources/AppsPanelSDKExtension"
-        ),
+            .target(
+                name: "AppsPanelSDK",
+                dependencies: [
+                    .product(name: "Alamofire", package: "Alamofire"),
+                    .product(name: "KeychainAccess", package: "KeychainAccess"),
+                    .product(name: "SwiftJWT", package: "Swift-JWT"),
+                    .product(name: "DatadogCore", package: "dd-sdk-ios"),
+                    .product(name: "DatadogLogs", package: "dd-sdk-ios")
+                ],
+                path: "Sources/AppsPanelSDK",
+                resources: [.process("Resources")]
+            ),
+            .target(
+                name: "AppsPanelSDKExtension",
+                dependencies: [
+                    "AppsPanelSDK",
+                ],
+                path: "Sources/AppsPanelSDKExtension"
+            ),
         .testTarget(
             name: "AppsPanelSDKv5PackageTests",
             dependencies: ["AppsPanelSDK"]),
